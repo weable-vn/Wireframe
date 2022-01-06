f15c37686320fa7a7bc965c2bdafb4d06eaf4761
duclinh21122000 <duongduclinh2207@gmail.com>
2022-01-06 18:56:09 +0700

    Process Website 06/01/2020

---
+++ assets/js/huy.js
@@ -0,0 +1,77 @@
+ /*
+                      |--------------------------------------------------------------------------
+                      | Custom JS
+                      |--------------------------------------------------------------------------
+                      |
+                      | About author: https://Sticky.vn/
+                      |
+                      */
+
+ /* ------------------------------------------------------------------------ *  
+     Sticky header when scroll
+ * ------------------------------------------------------------------------ */
+ // jQuery(document).ready(function () {
+ //   $(window).scroll(function(){
+ //     var scroll = $(window).scrollTop();
+ //     if (scroll > 62) {
+ //       $(".header").addClass("sticky");
+ //     }
+ //     else {
+ //       $(".header").removeClass("sticky");
+ //     }
+ //   });
+ // });
+
+ /* ------------------------------------------------------------------------ *  
+     jQuery inview
+ * ------------------------------------------------------------------------ */
+ // ('div').on('inview', function(event, isInView) {
+ //   if (isInView) {
+ //   } else {
+ //   }
+ // });
+
+
+
+ $(document).ready(function() {
+
+
+     $('.list-event').slick({
+         dots: false,
+         infinite: false,
+         speed: 300,
+         slidesToShow: 3,
+         slidesToScroll: 1,
+         responsive: [{
+                 breakpoint: 991,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 1,
+                     infinite: true,
+                     dots: true
+                 }
+             },
+             {
+                 breakpoint: 768,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 1
+                 }
+             },
+             {
+                 breakpoint: 480,
+                 settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 1
+                 }
+             }
+             // You can unslick at a given breakpoint now by adding:
+             // settings: "unslick"
+             // instead of a settings object
+         ]
+     });
+
+
+
+
+ })
